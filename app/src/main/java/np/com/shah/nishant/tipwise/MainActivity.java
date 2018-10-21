8e037b03bfdfbf48258885682de3da458d823db6
Nishant <nizant4@gmail.com>
2018-10-21 13:27:51 -0500

    Initial commit

---
+++ app/src/main/java/np/com/shah/nishant/tipwise/MainActivity.java
@@ -0,0 +1,36 @@
+package np.com.shah.nishant.tipwise;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void calculate(View v){
+        EditText text = (EditText)findViewById(R.id.amount);
+        String sr = text.getText().toString();
+        
+        TextView show = (TextView) findViewById(R.id.showbill);
+        show.setText(sr);
+
+        }
+
+
+
+
+
+
+
+
+
+}
